f0cae5d1d444bdd75af7b54dcc4f883e5df262b7
jalilraziel <idv16c.jzavala@uartesdigitales.edu.mx>
2018-09-26 14:58:40 -0500

    1.0-Subiendo el proyecto

---
+++ puntaje_de_tenis/Form1.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace puntaje_de_tenis
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.label1.Location = new System.Drawing.Point(26, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(153, 108);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "00";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.label2.Location = new System.Drawing.Point(185, 27);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(153, 108);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "00";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.label3.Location = new System.Drawing.Point(344, 27);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(153, 108);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "00";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.label4.Location = new System.Drawing.Point(26, 230);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(153, 108);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "00";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.label5.Location = new System.Drawing.Point(185, 230);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(153, 108);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "00";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.label6.Location = new System.Drawing.Point(344, 230);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(153, 108);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "00";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.label7.Location = new System.Drawing.Point(60, 156);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(67, 37);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Set";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.label8.Location = new System.Drawing.Point(207, 156);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(108, 37);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Game";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.label9.Location = new System.Drawing.Point(364, 156);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(112, 37);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "Points";
+            // 
+            // button1
+            // 
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.button1.Location = new System.Drawing.Point(534, 43);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(83, 78);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "A";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.button2.Location = new System.Drawing.Point(534, 244);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(83, 78);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "B";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.FlatAppearance.BorderSize = 0;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.button3.Location = new System.Drawing.Point(520, 156);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(107, 40);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "atras";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(800, 363);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Maracador de tenis";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ puntaje_de_tenis/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace puntaje_de_tenis
+{
+    public partial class Form1 : Form
+    {
+        public string[] s_Puntaje = new string[5]; //points puede tener 5 diferentes puntajes
+        public int A = 0, B = 0; // Contadores
+
+        public struct Save { // Guardo del retroceso
+            public string save1, save2, save3, save4, save5, save6;
+            public bool jugador;
+        }
+        public Save save;//onjetos del guardado
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            s_Puntaje[0] = "00"; // valores del contador points
+            s_Puntaje[1] = "15";
+            s_Puntaje[2] = "30";
+            s_Puntaje[3] = "40";
+            s_Puntaje[4] = "Ad";
+            button3.Enabled = false; // inahabilitamos botn de retroceso
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            save.save1 = label1.Text; save.save2 = label2.Text; save.save3 = label3.Text; //Hacemos el save del puntaje actual
+            save.save4 = label4.Text; save.save5 = label5.Text; save.save6 = label6.Text;
+            save.jugador = false;
+            B++;
+            if (A == 4 && B == 4) //si amabos llegan a estar en ventaja "Ad" los regresa a 40
+            {
+                A--;
+                B--;
+                label3.Text = s_Puntaje[3];
+                label6.Text = s_Puntaje[3];
+            }
+            if (B == 5) { // si un jugador mete 5 equivale es un game
+                label5.Text = '0' + Convert.ToString(Convert.ToInt32(label5.Text) + 1);
+                B = 0;//regresamos poins a 00
+                A = 0;
+                if (Convert.ToInt32(label5.Text) == 6) { // si un jugador mete 5 equivale a un set
+                    label4.Text = '0' + Convert.ToString(Convert.ToInt32(label4.Text) + 1);
+                    label5.Text = s_Puntaje[0];//regresamos game a 00
+                    label2.Text = s_Puntaje[0];
+                    if (Convert.ToInt32(label4.Text) == 2) { // con 2 sets gana
+                        label6.Text = s_Puntaje[B]; //cambia el la informacion de la tabla antes del messegebox
+                        label4.Text = s_Puntaje[0];
+                        MessageBox.Show("EL jugador A a ganado", "Ganador");//mensaje de victoria
+                    }
+                }
+            }
+            label6.Text = s_Puntaje[B]; //un jugador puede modificar al otro 
+            label3.Text = s_Puntaje[A];
+            button3.Enabled = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            save.save1 = label1.Text; save.save2 = label2.Text; save.save3 = label3.Text;
+            save.save4 = label4.Text; save.save5 = label5.Text; save.save6 = label6.Text;
+            save.jugador = true;
+            A++;
+            if (A == 4 && B == 4){
+                A--;
+                B--;
+                label3.Text = s_Puntaje[3];
+                label6.Text = s_Puntaje[3];
+            }
+            if (A == 5) {
+                label2.Text = '0' + Convert.ToString(Convert.ToInt32(label2.Text) + 1);
+                A = 0;
+                B = 0;
+                if (Convert.ToInt32(label2.Text) == 6) {
+                    label1.Text = '0' + Convert.ToString(Convert.ToInt32(label1.Text) + 1);
+                    label2.Text = s_Puntaje[0];
+                    label5.Text = s_Puntaje[0];
+                    if (Convert.ToInt32(label1.Text) == 2){
+                        label3.Text = s_Puntaje[B];
+                        MessageBox.Show("EL jugador A a ganado", "Ganador");
+                        label1.Text = s_Puntaje[0];
+                        button3.Enabled = false;
+                    }
+                }
+            }
+            label3.Text = s_Puntaje[A];
+            label6.Text = s_Puntaje[B];
+            button3.Enabled = true;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            label1.Text = save.save1; label2.Text = save.save2; label3.Text = save.save3;
+            label4.Text = save.save4; label5.Text = save.save5; label6.Text = save.save6;
+            if (save.jugador == true) A--; //jugador "A" metio el ultimo punto
+            else B--; //jugador "A" metio el ultimo punto
+ 
+            button3.Enabled = false;
+        }
+    }
+}
+
